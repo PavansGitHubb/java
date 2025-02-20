74a8f3429270322df1a3c7d57b2a2d5bee796481
Pavan kumar <136694590+PavansGitHubb@users.noreply.github.com>
2025-02-20 16:14:35 +0530

    Create restart_server.sh

---
+++ scripts/restart_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Restarting application..."
+
+# Stop the existing process
+sudo pkill -f "node" || true
+
+# Move new deployment
+sudo rm -rf /home/ubuntu/frontend
+sudo mv /home/ubuntu/deploy/frontend /home/ubuntu/frontend
+
+# Start the app
+cd /home/ubuntu/frontend
+nohup npm start > /dev/null 2>&1 &

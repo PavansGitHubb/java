d73117570f3c29eab37036e74642de9100af690d
Pavan kumar <136694590+PavansGitHubb@users.noreply.github.com>
2025-02-20 16:40:43 +0530

    Update restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-echo "Restarting application..."
-
-# Stop existing Node.js process
-sudo pkill -f "node" || true
-
-# Move the new build to the application directory
-sudo rm -rf /home/ubuntu/frontend
-sudo mv /home/ubuntu/deploy/frontend /home/ubuntu/frontend
-
-# Start the application
-cd /home/ubuntu/frontend
-nohup npm start > /dev/null 2>&1 &
+sudo systemctl restart my_application_service

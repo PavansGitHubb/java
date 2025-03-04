093c43a97fbc2b4383c04d798854cf047c801b67
Pavan kumar <136694590+PavansGitHubb@users.noreply.github.com>
2025-03-05 01:23:06 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+set -e
+echo "Cleaning old application files..."
 sudo rm -rf /home/ubuntu/angular-app/*
-sudo tar -xzf /home/ubuntu/angular-app/build.tar.gz -C /home/ubuntu/angular-app/
-sudo systemctl restart nginx
